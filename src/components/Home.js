7d38379ceeaff2bd90e8892fc1be5f88fc1c12e7
Nicole Lee <njlee@stanford.edu>
2024-04-30 13:44:29 -0700

    polished homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,6 +14,7 @@ import {
 // import { addDoc, collection } from "firebase/firestore";
 
 import whatsIncludedBg from "../images/whats-included-bg.png";
+import whatsIncludedBigBg from "../images/whats-included-big-bg.png";
 import downArrow from "../images/down-arrow.png";
 import leftMacbook from "../images/left.png";
 import rightMacbook from "../images/right.png";
@@ -95,11 +96,11 @@ function Home() {
             >
               Virgil
             </button>
-            <Link to="about-us" className="nav-link">
-              Our Product
-            </Link>
             <Link to="how-it-works" className="nav-link">
               How it works
+            </Link>
+            <Link to="join-our-waitlist" className="nav-link">
+              What's included
             </Link>
             <Link to="explore-demo" className="nav-link">
               Explore Demo
@@ -116,29 +117,23 @@ function Home() {
         <Element name="about-us" className="centered-flexbox">
           <div className="about-us-flex-container">
             <section className="about-us-text-container">
-              <h1 style={{ fontSize: "48px" }}>
-                Preserve your <span className="gradient-text">legacy</span>,{" "}
+              <h1 style={{ fontSize: "48px", marginBottom: "20px" }}>
+                Preserve your <span className="gradient-text">grandma's</span>
                 <br />
-                One <span className="gradient-text">memory</span> at a time.
+                legacy. One <span className="gradient-text">memory</span> at a
+                time.
               </h1>
               <p className="feature-description">
-                AI-powered biographies to capture and curate your loved one's
-                life story.
+                The Virgil Vault is a digital biography that preserves your
+                loved one's life story - in their own voice.
               </p>
               <div className="about-us-buttons-container">
                 <Link
-                  to="explore-demo"
+                  to="join-our-waitlist"
                   className="cta-button"
                   style={{ backgroundColor: "#FFDE62" }}
                 >
-                  Explore Demo
-                </Link>
-                <Link
-                  to="how-it-works"
-                  className="cta-button"
-                  style={{ border: "2px solid black" }}
-                >
-                  How It Works
+                  Join Our Waitlist
                 </Link>
               </div>
               <div className="about-us-scroll">
@@ -251,108 +246,118 @@ function Home() {
               <div
                 className="virgil-vault"
                 style={{
-                  backgroundImage: `url(${whatsIncludedBg})`,
-                  backgroundSize: "contain", // Cover ensures the background covers the whole area
+                  backgroundImage: `url(${whatsIncludedBigBg})`,
+                  backgroundSize: "cover cover", // Cover ensures the background covers the whole area
                   backgroundPosition: "center", // Center the image within the element
                   backgroundRepeat: "no-repeat",
                 }}
               >
-                <div className="virgil-vault-header">
-                  <h1 style={{ fontSize: "24px" }}>Virgil Vault</h1>
-                  <p>For your loved one</p>
-                  <h1
-                    clasName="virgil-vault-price"
-                    style={{ fontSize: "36px", marginTop: "30px" }}
-                  >
-                    $3950
-                  </h1>
-                </div>
-                <div className="virgil-vault-includes">
-                  <h3>Includes:</h3>
+                <div className="virgil-vault-holder">
+                  <div className="virgil-vault-header-holder">
+                    <div className="virgil-vault-header">
+                      <h1 style={{ fontSize: "24px" }}>Virgil Vault</h1>
+                      <p>For your loved one</p>
+                      <h1
+                        clasName="virgil-vault-price"
+                        style={{ fontSize: "36px", marginTop: "30px" }}
+                      >
+                        $3950
+                      </h1>
+                    </div>
+                  </div>
+                  <div className="virgil-vault-includes">
+                    <h3>Includes:</h3>
 
-                  <ul>
-                    <li>
-                      10 hours of conversational interviews — enough to capture
-                      their entire life story!
-                    </li>
-                    <li>High-fidelity audio and video recordings</li>
-                    <li>
-                      Virgil Vault — an interactive archive that merges their
-                      voice with photos, videos, and more
-                    </li>
-                    <li>
-                      Searchable video library to easily find specific memories
-                    </li>
-                    <li>
-                      A personal historian who will guide your family through
-                      the entire process
-                    </li>
-                    <li>Safe, secure, and private to you</li>
-                    <li>Shareable and downloadable</li>
-                  </ul>
+                    <ul>
+                      <li>
+                        10 hours of conversational interviews — enough to
+                        capture their entire life story!
+                      </li>
+                      <li>High-fidelity audio and video recordings</li>
+                      <li>
+                        Virgil Vault — an interactive archive that merges their
+                        voice with photos, videos, and more
+                      </li>
+                      <li>
+                        Searchable video library to easily find specific
+                        memories
+                      </li>
+                      <li>
+                        A personal historian who will guide your family through
+                        the entire process
+                      </li>
+                      <li>Safe, secure, and private to you</li>
+                      <li>Shareable and downloadable</li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-              <div className="waitlist-form-container">
-                <div className="waitlist-form-header">
-                  <h1 style={{ marginBottom: "10px" }}>
-                    Don't wait. Capture their story today.
-                  </h1>
-                  <p style={{ marginBottom: "15px" }}>
-                    {" "}
-                    Virgil is currently at capcity. Join the waitlist and we'll
-                    reach out when space becomes available.{" "}
-                  </p>
+              <div
+                className="waitlist-form-container"
+                style={{ paddingTop: "20px" }}
+              >
+                <div className="waitlist-container">
+                  <div className="waitlist-form-header">
+                    <h1 style={{ marginBottom: "10px" }}>
+                      Don't wait. Capture their story today.
+                    </h1>
+                    <p style={{ marginBottom: "15px" }}>
+                      {" "}
+                      Virgil is currently at capcity. Join the waitlist and
+                      we'll reach out when space becomes available.{" "}
+                    </p>
+                  </div>
+                  <form onSubmit={handleSubmit} className="waitlist-form">
+                    <label>
+                      Full Name *
+                      <input
+                        type="text"
+                        value={fullName}
+                        onChange={handleChangeName}
+                        required
+                      />
+                    </label>
+                    <label>
+                      Email Address *
+                      <input
+                        type="email"
+                        value={email}
+                        onChange={handleChangeEmail}
+                        required
+                      />
+                    </label>
+                    <label>
+                      Phone Number *
+                      <input
+                        type="tel"
+                        value={phoneNumber}
+                        onChange={handleChangeNumber}
+                        required
+                      />
+                    </label>
+                    <label>
+                      Whose story would you like to capture?
+                      {/** make default value blank*/}
+                      <select>
+                        <option selected value=""></option>
+                        <option value="grapefruit">Grapefruit</option>
+                        <option value="lime">Lime</option>
+                        <option value="mango">Mango</option>
+                      </select>
+                    </label>
+                    <label>
+                      Why are you interested in capturing their story?
+                      <input
+                        type="motivation"
+                        value={motivation}
+                        onChange={handleChangeMotivation}
+                      />
+                    </label>
+                    <button type="submit" className="waitlist-button">
+                      Join Our Waitlist
+                    </button>
+                  </form>
                 </div>
-                <form onSubmit={handleSubmit} className="waitlist-form">
-                  <label>
-                    Full Name *
-                    <input
-                      type="text"
-                      value={fullName}
-                      onChange={handleChangeName}
-                      required
-                    />
-                  </label>
-                  <label>
-                    Email Address *
-                    <input
-                      type="email"
-                      value={email}
-                      onChange={handleChangeEmail}
-                      required
-                    />
-                  </label>
-                  <label>
-                    Phone Number *
-                    <input
-                      type="tel"
-                      value={phoneNumber}
-                      onChange={handleChangeNumber}
-                      required
-                    />
-                  </label>
-                  <label>
-                    Whose story would you like to capture?
-                    {/** make default value blank*/}
-                    <select>
-                      <option selected value=""></option>
-                      <option value="grapefruit">Grapefruit</option>
-                      <option value="lime">Lime</option>
-                      <option value="mango">Mango</option>
-                    </select>
-                  </label>
-                  <label>
-                    Why are you interested in capturing their story?
-                    <input
-                      type="motivation"
-                      value={motivation}
-                      onChange={handleChangeMotivation}
-                    />
-                  </label>
-                  <button type="submit" className="waitlist-button">
-                    Join Our Waitlist
-                  </button>
-                </form>
               </div>
             </div>
           </div>
@@ -360,7 +365,7 @@ function Home() {
         <Element name="explore-demo" className="centered-flexbox">
           <div
             style={{
-              backgroundColor: "#FFEFB5",
+              backgroundColor: "#FFDE62",
               borderRadius: "20px",
               height: "80%",
               width: "80%",
